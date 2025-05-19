ba7b9043f470d55582c22425ba658307700c4c78
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-05-20 01:44:49 +0300

    Removing the comments

--- app.py
+++ app.py
@@ -14,14 +14,12 @@ import nltk
 from nltk.corpus import stopwords
 from collections import Counter
 
-# Load vectorizer and model from disk
 with open("vectorizer.pkl", "rb") as f:
     vectorizer = pickle.load(f)
 
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
-# Streamlit app UI
 st.title("📧 Email/SMS Spam Classifier")
 
 user_input = st.text_area("Enter Message", "")
@@ -30,15 +28,11 @@ if st.button("Check"):
     if not user_input.strip():
         st.warning("Please enter a message.")
     else:
-        # Convert user input to feature vector
         features = vectorizer.transform([user_input])
 
-        # Predict using the loaded model
         prediction = model.predict(features)
 
-        # Display result
         if prediction[0] == 1:
             st.success("✅ This is a **Ham Mail** (Not Spam).")
         else:
             st.error("🚫 This is a **Spam Mail**.")
-
